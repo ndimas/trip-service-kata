b00eddcffb82cd8af15529d2a701d98c46875f7f
nickdhima <ndimas.tech@gmail.com>
2023-11-03 13:33:01 +0100

    Part 1: Refactoring: Methode isFriend, Variable isLoggedInUserAFriend extrahiert, CollaboratorCallException entfernt
    Anforderung #1: You need to be logged in to see the content

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -12,8 +12,16 @@ public class TripService {
     UserSession userSession = UserSession.getInstance();
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<>();
         User loggedInUser = userSession.getLoggedInUser();
+        boolean isLoggedInUserAFriend = isFriend(user, loggedInUser);
+        boolean allowedToViewTrips = isLoggedInUserAFriend || loggedInUser.equals(user);
+        if (allowedToViewTrips) {
+            return TripDAO.findTripsByUser(user);
+        }
+        return new ArrayList<>();
+    }
+
+    private boolean isFriend(User user, User loggedInUser) {
         boolean isFriend = false;
         for (User friend : user.getFriends()) {
             if (friend.equals(loggedInUser)) {
@@ -21,9 +29,6 @@ public class TripService {
                 break;
             }
         }
-        if (isFriend || loggedInUser.equals(user)) {
-            tripList = TripDAO.findTripsByUser(user);
-        }
-        return tripList;
+        return isFriend;
     }
 }
