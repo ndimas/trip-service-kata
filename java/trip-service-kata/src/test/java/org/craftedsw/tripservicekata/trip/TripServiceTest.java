480e86857474f3b9d02e68d9fc50caa0a3a2b711
nickdhima <ndimas.tech@gmail.com>
2023-11-03 10:37:04 +0100

    Update Java and Spring versions

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,8 @@
 package org.craftedsw.tripservicekata.trip;
 
 public class TripServiceTest {
-	
+    private final TripService = new TripService();
+
+	// You need to be logged in to see the content
+
 }
